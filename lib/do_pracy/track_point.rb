f84fb6dd766b6470b79ae172e743d4a37b8d377e
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2014-10-15 07:29:51 +0200

    Addint TrackPoint and its tests

---
+++ lib/do_pracy/track_point.rb
@@ -0,0 +1,35 @@
+require 'tzinfo'
+
+module DoPracy
+
+	class TrackPoint
+
+		attr_reader :time
+		attr_reader	:lon
+		attr_reader	:lat
+		attr_reader	:type
+
+		def initialize (time, lon, lat, type)
+			@time = time
+			@lon = lon
+			@lat = lat
+			@type = type
+		end
+
+		def self.from_node node
+			lat = node.attribute("lat").content.to_f
+			lon = node.attribute("lon").content.to_f
+			time_str = node.xpath("gpx:time","gpx" => "http://www.topografix.com/GPX/1/1").first
+			type_str = node.xpath("gpx:type","gpx" => "http://www.topografix.com/GPX/1/1").first
+			time = Time.parse(time_str.content)
+			type = :unknown
+			type = type_str.content if type_str != nil
+
+			return TrackPoint.new time, lon, lat, type
+		end
+	end
+end
+
+
+      #<trkpt lat="54.500128" lon="18.448622">
+        #<time>2014-09-29T05:02:38Z</time>
---
+++ spec/track_point_tests.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'nokogiri'
+require './lib/do_pracy/track_point'
+
+module DoPracy
+
+	class When_creating_TrackPoint_from_xml_node < MiniTest::Test
+
+		def setup
+			doc_with_type = Nokogiri::XML(open('./spec/track_point_tests-with_type.xml'))
+			doc_without_type = Nokogiri::XML(open('./spec/track_point_tests-without_type.xml'))
+
+			node_with_type = doc_with_type.xpath("//gpx:trkpt", "gpx" => "http://www.topografix.com/GPX/1/1")
+			node_without_type = doc_without_type.xpath("//gpx:trkpt", "gpx" => "http://www.topografix.com/GPX/1/1")
+
+			@track_point_with_type = TrackPoint.from_node node_with_type
+			@track_point_without_type = TrackPoint.from_node node_without_type
+		end
+
+		def test_should_get_time_from_time_node
+			assert_equal Time.parse('2014-09-29T05:02:38Z'), @track_point_with_type.time
+		end
+
+		def test_should_get_lontitude_from_lon_attribute
+			assert_equal 18.448622, @track_point_with_type.lon
+		end
+
+		def test_should_get_latitude_from_lat_attribute
+			assert_equal 54.500128, @track_point_with_type.lat
+		end
+
+		def test_should_get_type_from_type_node
+			assert_equal "bicycle", @track_point_with_type.type
+		end
+
+		def test_should_return_unknown_type_when_type_is_not_provided
+			assert_equal :unknown, @track_point_without_type.type
+		end
+	end
+end
